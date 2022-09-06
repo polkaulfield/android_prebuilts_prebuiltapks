2c89032da540f091dccc09fece569016b86c2621
Pol Kaulfield <mr.tamuraa@gmail.com>
2022-09-06 12:03:53 +0200

    fixed small typo

--- BromiteWebView/Android.mk
+++ BromiteWebView/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := BromiteWebview
+LOCAL_MODULE := BromiteWebView
 LOCAL_SRC_FILES := arm_SystemWebView.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
